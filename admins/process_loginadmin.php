15386f96c2c796f4569c2044ff77cc539c47e63b
VuMinhHieu <minghieuu@gmail.com>
2022-01-11 17:01:34 +0700

    update

--- admins/process_loginadmin.php
+++ admins/process_loginadmin.php
@@ -8,13 +8,13 @@ if(isset($_POST['btnLogin'])){
     if(!$conn){
         die("kết nối thất bại!");
     }
-    $sql = "SELECT * FROM admins WHERE name = '$name' AND pass='$pass'";
+    $sql = "SELECT * FROM admins WHERE name = '$name'";
 
     
     $result = mysqli_query($conn,$sql);
    
     $rs=mysqli_fetch_array($result);
-    echo mysqli_num_rows($result);
+  
     if(mysqli_num_rows($result) > 0){
         $row = mysqli_fetch_assoc($result);
         
